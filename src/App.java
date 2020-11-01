909eb1531f674cf84bc28c809425d223f890052d
wisnubayuaji <wisnubayu34@gmail.com>
2020-11-01 07:27:23 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+/**
+ * @author Nim : 10116430 Nama : Wisnu Bayu Aji Kelas: IF-2
+ */
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int saldoAwal = 2500000;
+        double bunga = 0.15;
+
+        DecimalFormat kursIndo = (DecimalFormat) DecimalFormat.getCurrencyInstance();
+        DecimalFormatSymbols formatRp = new DecimalFormatSymbols();
+
+        formatRp.setCurrencySymbol("Rp.");
+        formatRp.setMonetaryDecimalSeparator(',');
+        formatRp.setGroupingSeparator('.');
+
+        for (int i = 1; i <= 6; i++) {
+            int saldoAkhir = (int) (saldoAwal + (saldoAwal * bunga));
+            
+            kursIndo.setDecimalFormatSymbols(formatRp);
+            System.out.println("Saldo di bulan ke-" + i + " " + kursIndo.format(saldoAkhir));
+            
+            saldoAwal = saldoAkhir;
+
+        }
     }
 }
